f0a0cf45e70b43262e1e4c03a9a6b4a54bd13783
Connor Holly <connorholly11@gmail.com>
2019-10-03 13:18:41 -0500

    starting over

--- index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const defaults = require("./api/configs/defaults");
-
-const server = express();
-
-server.listen(defaults.port, () => {
-  console.log(`API running on port ${defaults.port}`);
-});
